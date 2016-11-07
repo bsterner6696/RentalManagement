adfb08a4dcd07884ba6c4adbfa663d1501d69311
Benjamin <bsterner6696@gmail.com>
2016-11-07 15:11:18 -0600

    Made models for tenant, manager, apartment, and property

---
+++ RentalManagement/RentalManagement/Models/PropertyManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RentalManagement.Models
+{
+    public class PropertyManager
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ RentalManagement/RentalManagement/Models/Tenant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RentalManagement.Models
+{
+    public class Tenant
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Balance { get; set; }
+        [ForeignKey("Apartment")]
+        public int ApartmentId { get; set; }
+        public Apartment Apartment { get; set; }
+
+    }
+}
